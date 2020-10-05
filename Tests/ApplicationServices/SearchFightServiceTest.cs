395f0e143e36acb101156b5b1f7f8fda62e67816
Rodrigo Téllez Lazo <r.tellez91@outlook.com.pe>
2020-10-05 09:41:11 -0500

    Updating test cases and relocating sln file

--- Tests/ApplicationServices/SearchFightServiceTest.cs
+++ Tests/ApplicationServices/SearchFightServiceTest.cs
@@ -8,6 +8,7 @@ using SearchFight.Infrastructure.SearchProviders;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SearchFight.Tests.ApplicationServices
@@ -36,80 +37,80 @@ namespace SearchFight.Tests.ApplicationServices
         }
 
         [Fact]
-        public void Should_Search_Once_PerUniqueWord()
+        public async Task Should_Search_Once_PerUniqueWord()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
             googleProvider.Verify(gp => gp.Search(It.IsAny<SearchRequest>()), Times.Exactly(2));
             bingProvider.Verify(gp => gp.Search(It.IsAny<SearchRequest>()), Times.Exactly(2));
         }
 
         [Fact]
-        public void Should_AddResultBySearchTerm_Once_PerUniqueWord()
+        public async Task Should_AddResultBySearchTerm_Once_PerUniqueWord()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
-            searchFightResultsBuilder.Verify(builder => builder.AddResultBySearchTerm(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()), Times.Exactly(4));
+            searchFightResultsBuilder.Verify(builder => builder.AddResultBySearchTerm(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<long>()), Times.Exactly(4));
         }
 
         [Fact]
-        public void Should_AddWinnerByProvider_Once_PerUniqueWord()
+        public async Task Should_AddWinnerByProvider_Once_PerUniqueWord()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
             searchFightResultsBuilder.Verify(builder => builder.AddWinnerByProvider(It.IsAny<string>(), It.IsAny<string>()), Times.Exactly(2));
         }
 
         [Fact]
-        public void Should_SetFightWinnerByProvider_Once()
+        public async Task Should_SetFightWinnerByProvider_Once()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
             searchFightResultsBuilder.Verify(builder => builder.SetSearchFightWinner(It.IsAny<string>()), Times.Once);
         }
 
         [Fact]
-        public void Should_GetSearchFightWinner_Once()
+        public async Task Should_GetSearchFightWinner_Once()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
             searchFightDomainService.Verify(service => service.GetSearchFightWinner(It.IsAny<IEnumerable<SearchFightProvider>>()), Times.Once);
         }
 
         [Fact]
-        public void Should_Build_SearchFightResults_Once()
+        public async Task Should_Build_SearchFightResults_Once()
         {
-            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
-            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).Returns(new SearchResult());
+            googleProvider.Setup(gp => gp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
+            bingProvider.Setup(bp => bp.Search(It.IsAny<SearchRequest>())).ReturnsAsync(new SearchResult());
 
             var searchTerms = new[] { ".net", "java", "Java" };
 
-            searchFightService.RunFight(searchTerms);
+            await searchFightService.RunFight(searchTerms);
 
             searchFightResultsBuilder.Verify(builder => builder.Build(), Times.Once);
         }
